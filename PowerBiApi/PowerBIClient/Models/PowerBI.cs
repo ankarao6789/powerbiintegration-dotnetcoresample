ff38981f2c647afab33ad258611205c7dbaeabd0
apotla <apotla@agility.com>
2026-01-28 11:41:14 +0530

    commit changes

---
+++ PowerBiApi/PowerBIClient/Models/PowerBI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agility.OPSX.Api.PowerBIClient.Models
+{
+    public class PowerBI
+    {
+
+        // Workspace Id for which Embed token needs to be generated
+        public string WorkspaceId { get; set; }
+
+        // Report Id for which Embed token needs to be generated
+        public string ReportId { get; set; }
+    }
+}
---
+++ PowerBiApi/PowerBIClient/PowerBIEmbedInfoController.cs
@@ -0,0 +1,66 @@
+﻿// <copyright file="PowerBIEmbedInfoController.cs" company="Agility E Services">
+// Copyright (c) Agility E Services. All rights reserved.
+// </copyright>
+
+namespace Agility.OPSX.Api.PowerBIClient
+{
+    using Agility.OPSX.Api.PowerBIClient.Models;
+    using Agility.OPSX.Api.PowerBIClient.Services;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Options;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.Json;
+    using System.Threading.Tasks;
+
+    [AllowAnonymous]
+    [Route("api/PowerBIEmbedInfo")]
+    public class PowerBIEmbedInfoController : Controller
+    {
+        private readonly PbiEmbedService pbiEmbedService;
+        private readonly IOptions<AzureAd> azureAd;
+        private readonly IOptions<PowerBI> powerBI;
+
+        public PowerBIEmbedInfoController(PbiEmbedService pbiEmbedService, IOptions<AzureAd> azureAd, IOptions<PowerBI> powerBI)
+        {
+            this.pbiEmbedService = pbiEmbedService;
+            this.azureAd = azureAd;
+            this.powerBI = powerBI;
+        }
+
+        /// <summary>
+        /// Returns Embed token, Embed URL, and Embed token expiry to the client
+        /// </summary>
+        /// <returns>JSON containing parameters for embedding</returns>
+        [HttpGet]
+        [Route("GetEmbedInfo")]
+        public string GetEmbedInfo()
+        {
+            try
+            {
+                // Validate whether all the required configurations are provided in appsettings.json
+                string configValidationResult = ConfigValidatorService.ValidateConfig(azureAd, powerBI);
+                if (configValidationResult != null)
+                {
+                    HttpContext.Response.StatusCode = 400;
+                    return configValidationResult;
+                }
+                List<Guid> reports = new List<Guid>();
+                
+                reports.Add(new Guid("ee29977f-17b5-47b7-a77c-b2d3e49a706d"));
+                reports.Add(new Guid("a32c45af-9500-4cbc-b582-e5871f8a2bfd"));
+                //EmbedParams embedParams = pbiEmbedService.GetEmbedParams(new Guid(powerBI.Value.WorkspaceId), new Guid(powerBI.Value.ReportId));
+                EmbedParams embedParams = pbiEmbedService.GetEmbedParams(new Guid(powerBI.Value.WorkspaceId), reports);
+                return JsonSerializer.Serialize<EmbedParams>(embedParams);
+            }
+            catch (Exception ex)
+            {
+                HttpContext.Response.StatusCode = 500;
+                return ex.Message + "\n\n" + ex.StackTrace;
+            }
+        }
+
+    }
+}
